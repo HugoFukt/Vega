6487d7bc93a4053324acc44c4ea5dbdab755489a
YuXinyang <1048612051@qq.com>
2018-09-17 10:47:06 +0800

    vega project

---
+++ src/main/java/Phantom/Vega/App.java
@@ -0,0 +1,13 @@
+package Phantom.Vega;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
