ef41f97f6720adcb5001c8150b7f4ff110dfd295
YuXinyang <1048612051@qq.com>
2018-09-17 16:45:18 +0800

    change

--- src/main/java/Phantom/Vega/App.java
+++ src/main/java/Phantom/Vega/App.java
@@ -4,12 +4,10 @@ package Phantom.Vega;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello Jenkins!" );
-        System.out.println( "Hello Vega!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello Jenkins!");
+		System.out.println("Hello Vega!");
+	}
 }
