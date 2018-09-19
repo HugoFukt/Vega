68ca73a15fb4629cb896878cb6a5cf7b586e5f69
YuXinyang <1048612051@qq.com>
2018-09-19 17:40:06 +0800

    change

--- src/main/java/Phantom/Vega/App.java
+++ src/main/java/Phantom/Vega/App.java
@@ -9,6 +9,5 @@ public class App {
 		System.out.println("Hello World!");
 		System.out.println("Hello Jenkins!");
 		System.out.println("Hello Vega!");
-		System.out.println("Hello HugoFukt!");
 	}
 }
